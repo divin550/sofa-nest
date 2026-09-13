be1f878f923c85949cca47a74d8b15bccf073a2b
divin550 <mohsinqadir14@gmail.com>
2026-09-13 21:12:14 +0500

    style compelet of price dropdown manu

--- product.js
+++ product.js
@@ -1,18 +1,18 @@
 const modeBtn = document.getElementById("mode")
 const cartBtn = document.querySelector(".cart")
 const cartCount = document.querySelector(".cart-count")
-const cartMode = document.querySelector(".chart-mode")
+const rangeInput = document.querySelector("form-range")
 
+rangeInput.addEventListener("input", (e)=>{
+    
+    
 
-modeBtn.addEventListener("click", (e)=>{
-    const body = document.querySelector("body")
-    body.classList.toggle("black")
-    if (body.classList.contains("black") === true) {
-        cartMode.classList.add("white")
-
-        
-    }
+    
 
 })
+
+
+
+
 
 
