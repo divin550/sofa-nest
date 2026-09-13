ef53d58bb8da9c2003b30c6d5976039281a97cfb
divin550 <mohsinqadir14@gmail.com>
2026-09-13 23:32:41 +0500

    added javascript to range input to filter price

--- product.js
+++ product.js
@@ -1,18 +1,19 @@
-const modeBtn = document.getElementById("mode")
-const cartBtn = document.querySelector(".cart")
-const cartCount = document.querySelector(".cart-count")
-const rangeInput = document.querySelector("form-range")
+const modeBtn = document.getElementById("mode");
+const cartBtn = document.querySelector(".cart");
+const cartCount = document.querySelector(".cart-count");
+const rangeInput = document.getElementById("form-range");
+const rangeInput2 = document.getElementById("form-range2");
 
-rangeInput.addEventListener("input", (e)=>{
-    
-    
-
-    
-
-})
-
-
-
-
-
-
+rangeInput.addEventListener("input", (e) => {
+  const minPrice = document.getElementById("price-min");
+  if (rangeInput.value) {
+    minPrice.innerHTML = rangeInput.value;
+  }
+});
+rangeInput2.addEventListener("input", (e) => {
+    const maxPrice = document.getElementById("price-max")
+    if (rangeInput2) {
+        maxPrice.innerHTML = rangeInput2.value
+        
+    }
+});
